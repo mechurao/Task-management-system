a69fdced100c8fa8ef0ede32dc8791315d0e80aa
Ondřej Měchura <mechurao1998@gmail.com>
2024-06-10 11:52:58 +0200

    API definitions completed

--- src/main/java/com/mechurao/taskmanagementsystem/api/TaskService.java
+++ src/main/java/com/mechurao/taskmanagementsystem/api/TaskService.java
@@ -1,8 +1,20 @@
 package com.mechurao.taskmanagementsystem.api;
 
-import com.mechurao.taskmanagementsystem.api.request.UserAddRequest;
+import com.mechurao.taskmanagementsystem.api.request.TaskAddRequest;
+import com.mechurao.taskmanagementsystem.api.request.TaskEditRequest;
+import com.mechurao.taskmanagementsystem.domain.Task;
+import com.mechurao.taskmanagementsystem.domain.TaskStatus;
+
+import java.util.List;
 
 public interface TaskService {
-    long add(UserAddRequest request);
-    void edit(long id, )
+    long add(TaskAddRequest request);
+    void edit(long id, TaskEditRequest request);
+    void changeStatus(long id, TaskStatus status);
+    void assignProject(long taskId, long projectId);
+    void delete(long id);
+    Task get(long id);
+    List<Task> getAll();
+    List<Task> getAllByUserId(Long userId);
+    List<Task> getAllByProjectId(Long projectId);
 }
--- src/main/java/com/mechurao/taskmanagementsystem/api/request/TaskEditRequest.java
+++ src/main/java/com/mechurao/taskmanagementsystem/api/request/TaskEditRequest.java
@@ -1,4 +1,15 @@
 package com.mechurao.taskmanagementsystem.api.request;
 
+import com.mechurao.taskmanagementsystem.domain.TaskStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskEditRequest {
+    private String name;
+    private String description;
+    private TaskStatus status;
 }
