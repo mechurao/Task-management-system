2a99a93126ac92d0a27f5d19b6d521d412c252ae
Ondřej Měchura <mechurao1998@gmail.com>
2024-06-14 19:59:14 +0200

    Task entity added

---
+++ src/main/java/com/mechurao/taskmanagementsystem/implementation/jpa/entity/TaskEntity.java
@@ -0,0 +1,53 @@
+package com.mechurao.taskmanagementsystem.implementation.jpa.entity;
+
+import com.mechurao.taskmanagementsystem.domain.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Entity(name = "task")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskEntity {
+    @Id
+    @SequenceGenerator(name = "task_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "task_id_seq")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private ProjectEntity project;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = true)
+    @Setter
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Setter
+    private TaskStatus status;
+
+    @Column(nullable = false)
+    private OffsetDateTime createdAt;
+
+    public TaskEntity(UserEntity user, ProjectEntity project, String name, String description, TaskStatus status, OffsetDateTime createdAt) {
+        this.user = user;
+        this.project = project;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+    }
+}
